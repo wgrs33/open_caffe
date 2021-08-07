4b26fafc4cc6f8c435441a00db630ae2d38f4238
Patrick Wagner <patrick.richard.wagner@gmail.com>
2021-08-07 19:11:19 +0200

    [DEV] Added InputDevice and OutputDevice

---
+++ sdk/lib/include/opencaffe/base/devices/inputdevice.h
@@ -0,0 +1,28 @@
+#ifndef SDK_CORE_BASE_INPUT_DEVICE_H
+#define SDK_CORE_BASE_INPUT_DEVICE_H
+
+#include "opencaffe/base/base.h"
+
+namespace OpenCaffe {
+
+class InputDevice {
+public:
+    enum class State {
+        OFF = 0,
+        ON,
+        ERROR
+    };
+
+    InputDevice(uint8_t id) :
+    id_(id) {}
+    ~InputDevice();
+
+    State get_state();
+private:
+    uint8_t id_;
+    State state_;
+};
+
+} //namespace OpenCaffe
+
+#endif //SDK_CORE_BASE_INPUT_DEVICE_H
---
+++ sdk/lib/include/opencaffe/base/devices/outputdevice.h
@@ -0,0 +1,31 @@
+#ifndef SDK_CORE_BASE_OUTPUT_DEVICE_H
+#define SDK_CORE_BASE_OUTPUT_DEVICE_H
+
+#include "opencaffe/base/base.h"
+
+namespace OpenCaffe {
+
+class OutputDevice {
+public:
+    enum class State {
+        OFF = 0,
+        ON,
+        ERROR
+    };
+
+    OutputDevice(uint8_t id, State default_state = State::OFF) :
+    id_(id),
+    state_(default_state) {}
+    ~OutputDevice();
+
+    int on();
+    int off();
+    State get_state();
+private:
+    uint8_t id_;
+    State state_;
+};
+
+} //namespace OpenCaffe
+
+#endif //SDK_CORE_BASE_OUTPUT_DEVICE_H
